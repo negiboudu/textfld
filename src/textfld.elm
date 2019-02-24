80823c090bd92a86218dccbaadd0d354bfe73bce
EC2 Default User <ec2-user@ip-172-26-7-12.ap-northeast-1.compute.internal>
2019-02-24 04:48:49 +0000

    add tr and td

--- src/textfld.elm
+++ src/textfld.elm
@@ -1,7 +1,7 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Attribute, Html, div, input, table, text)
+import Html exposing (Attribute, Html, div, input, table, td, text, tr)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
@@ -52,5 +52,5 @@ view model =
     div []
         [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
         , div [] [ text (String.reverse model.content) ]
-        , table [] []
+        , table [] [ tr [] [ td [] [ text "aa" ] ] ]
         ]
